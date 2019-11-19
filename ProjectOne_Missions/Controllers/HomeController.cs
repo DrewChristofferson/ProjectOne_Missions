61e78ff7af5c2f7aac9b6d4c71fe4aafb52cc308
Andrew Christofferson <andrew.christofferson97@gmail.com>
2019-11-19 08:25:34 -0700

    Added a list of strings myList

--- ProjectOne_Missions/Controllers/HomeController.cs
+++ ProjectOne_Missions/Controllers/HomeController.cs
@@ -8,10 +8,12 @@ namespace ProjectOne_Missions.Controllers
 {
     public class HomeController : Controller
     {
+        public static List<string> myList = new List<string>();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
